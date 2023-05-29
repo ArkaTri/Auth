27caec5ab47dcf2075b08c8b56e3586c3dbb98d6
ArkaTri <cakra.wasana.cw@gmail.com>
2023-05-29 21:56:34 +0700

    lalala

--- src/components/GoogleLogin.jsx
+++ src/components/GoogleLogin.jsx
@@ -1,13 +1,20 @@
 import { useGoogleLogin } from '@react-oauth/google'
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import { googleLogin } from '../redux/actions/auth'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
 const GoogleLogin = ({buttonText}) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const login = useGoogleLogin({
         onSuccess: (tokenResponse) => {
-            console.log("dah ada tokennya")
-            console.log(tokenResponse)
+            const data = {
+                access_token: tokenResponse.access_token,
+            }
+            dispatch(googleLogin(data, navigate))
         }
     })
 
